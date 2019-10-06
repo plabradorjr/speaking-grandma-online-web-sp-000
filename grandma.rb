ee69e2468ae6574edb4d3c0aec1e8ed16ab55b28
Pancho Labrador <plabradorjr@gmail.com>
2019-10-06 17:13:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
@@ -14,7 +14,7 @@
 def speak_to_grandma(x)
   if x == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif x != x.upcase 
+  elsif x != x.upcase
     return "HUH?! SPEAK UP, SONNY!"
   elsif x == x.upcase
     return "NO, NOT SINCE 1938!"
